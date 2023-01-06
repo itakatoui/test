b3f7b5389eec00eb7ed304903bca0e4fc635480d
itakatoui <121990209+itakatoui@users.noreply.github.com>
2023-01-06 00:15:00 +0000

    Add files via upload

---
+++ import streamlit as st.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Happy New Year!")
+st.markdown("""
+# Welcome to the New Year
+
+Here's to hoping that the new year brings you joy, health, and prosperity.
+""")
+
+st.balloons()
